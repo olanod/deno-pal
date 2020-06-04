8cdf046398ed58712da5f910f4261f0320a2b506
Daniel Olano <daniel@olanod.com>
2020-06-04 10:13:53 +0200

    Add command args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(about = "Deno's best friend")]
+enum Args {
+    /// Print useful information
+    Info,
+    /// Initializes the current dir as a deno project
+    Init,
+    /// Adds a dependency to the project
+    Add { name: String },
+}
+
+#[paw::main]
+fn main(args: Args) {
+    match args {
+        Args::Info => println!("Not much info for now"),
+        Args::Init => println!("Doing ... nothing!"),
+        Args::Add { name } => println!("Feeling lazy, {} is a nice name though", name),
+    }
 }
