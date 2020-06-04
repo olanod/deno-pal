0c0764e5337cdb1fce06002a7aa3916d0221c9e1
Daniel Olano <daniel@olanod.com>
2020-06-04 11:43:39 +0200

    Create import map with init command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use async_std::{fs::File, io, path::Path, prelude::*};
+use serde_json::{json, to_vec_pretty};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -11,11 +13,39 @@ enum Args {
     Add { name: String },
 }
 
+#[async_std::main]
 #[paw::main]
-fn main(args: Args) {
+async fn main(args: Args) -> io::Result<()> {
     match args {
-        Args::Info => println!("Not much info for now"),
-        Args::Init => println!("Doing ... nothing!"),
-        Args::Add { name } => println!("Feeling lazy, {} is a nice name though", name),
+        Args::Info => info().await?,
+        Args::Init => init().await?,
+        Args::Add { name } => add(&name).await?,
     }
+    Ok(())
+}
+
+async fn init() -> io::Result<()> {
+    let imports_path = Path::new("import_map.json");
+    if imports_path.exists().await {
+        return Ok(());
+    }
+    let imports = to_vec_pretty(&json!({
+        "imports": {
+            "@std": "https://deno.land/std/"
+        }
+    }))?;
+    File::create(imports_path)
+        .await?
+        .write_all(&imports)
+        .await?;
+    Ok(())
+}
+
+async fn info() -> io::Result<()> {
+    println!("Not much info for now");
+    Ok(())
+}
+async fn add(name: &str) -> io::Result<()> {
+    println!("Feeling lazy, {} is a nice name though", name);
+    Ok(())
 }
